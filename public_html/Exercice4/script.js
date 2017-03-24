17c5461d1223515a20f4c3ec1eadc1bcaa0b4ba1
Valerie Jardin <valerie.jardin60@gmail.com>
2017-03-24 10:00:09 +0100

    Partie 2 d'AngularJS terminée

---
+++ public_html/Exercice4/script.js
@@ -0,0 +1,10 @@
+var schoolApp = angular.module('schoolApp',[]);
+
+schoolApp.controller('studentsCtrl',function($scope){
+  $scope.firstName = 'Émilie';
+  $scope.lastName = 'Gérard';
+  $scope.age = '17';
+  $scope.grade = 'Terminale';
+});
+
+
